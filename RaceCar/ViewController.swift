10b35fcd9db0e0d49617f9a5f79ff1f18e895b95
Thomas Tay <chengyc@live.com>
2015-07-16 23:04:50 +0800

    Initial Commit

---
+++ RaceCar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RaceCar
+//
+//  Created by Thomas Cheng on 16/7/15.
+//  Copyright © 2015 Thomas Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RaceCarTests/RaceCarTests.swift
@@ -0,0 +1,36 @@
+//
+//  RaceCarTests.swift
+//  RaceCarTests
+//
+//  Created by Thomas Cheng on 16/7/15.
+//  Copyright © 2015 Thomas Cheng. All rights reserved.
+//
+
+import XCTest
+@testable import RaceCar
+
+class RaceCarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
